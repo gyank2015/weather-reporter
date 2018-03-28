0c3fe394e5586bc185fa412138952dd2249f511e
gyank2015 <31079194+gyank2015@users.noreply.github.com>
2018-03-28 10:23:08 +0530

    Update geocode.js

--- geocode.js
+++ geocode.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const moment =require('moment');
 
 
 var geocodeAddress = (address,callback)=>{
@@ -22,16 +23,24 @@ request({
       url: `https://api.darksky.net/forecast/f67ea4a28217b5aec90d9459c395322d/${JSON.stringify(body.results[0].geometry.location.lat)},${JSON.stringify(body.results[0].geometry.location.lng)}`,
       json :true
     },(error,response,body)=>{
-      var results ={
+      if(error)
+        callback('Unable to connect to Forecast Servers.',undefined);
+      else if(error==="The given location is invalid.")
+        callback('The given location is invalid.',undefined);
+      else{
+        var results ={
         Address: savedAddress,
+        currentTime:moment(body.currently.time*1000).format("DD-MM-YYYY h:mm:ss"),
         currentTemperature : JSON.stringify(body.currently.temperature)+'F',
         apparentTemperature : JSON.stringify(body.currently.apparentTemperature)+'F',
         weatherType: body.currently.summary,
         humidity :JSON.stringify(body.currently.humidity*100)+'%',
-        windSpeed: JSON.stringify(body.currently.windSpeed)+'mph'
+        windSpeed: JSON.stringify(body.currently.windSpeed)+'mph',
+        Prediction:body.daily.summary
       }
       callback(undefined ,results);
-    });
+    }
+  });
 
     // console.log(`Address: ${JSON.stringify(body.results[0].formatted_address)}.`);
     // console.log(`Latitude: ${JSON.stringify(body.results[0].geometry.location.lat)}`);
